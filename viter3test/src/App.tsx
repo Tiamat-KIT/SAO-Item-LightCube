bdfcd479f2af5c96ee014a34c92f46b114666b78
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2023-07-04 07:44:25 +0000

    おしまい！！

--- viter3test/src/App.tsx
+++ viter3test/src/App.tsx
@@ -1,62 +1,66 @@
 import {OrbitControls,Plane,Stats, useCubeTexture} from "@react-three/drei"
 import {Canvas, useFrame, useThree} from "@react-three/fiber"
-import { Suspense, useRef,useState } from "react"
+import { Suspense, useEffect, useRef,useState } from "react"
 import * as three from "three"
 
 
-function CubeObj1(){
-  const boxRef = useRef<three.Mesh>(null!)
-  const loader = new three.TextureLoader()
+function App() {
+  function CubeObj1({bool,setbool,speed} : {bool: boolean,setbool:React.Dispatch<React.SetStateAction<boolean>>,speed:number}){
+    const boxRef = useRef<three.Mesh>(null!)
+    const loader = new three.TextureLoader()
+    const metalness = 2.0
+    
+  
+    const materials = [
+      new three.MeshStandardMaterial({map:loader.load("./normal/normal_top.png"),bumpMap: loader.load("./normal/normal_top.png"),bumpScale: 0.05,metalness: metalness}),
+      new three.MeshStandardMaterial({map:loader.load("./normal/normal_bottom.png"),bumpMap: loader.load("./normal/normal_bottom.png"),bumpScale: 0.05,metalness: metalness}),
+      new three.MeshStandardMaterial({map:loader.load("./normal/normal_front.png"),bumpMap: loader.load("./normal/normal_front.png"),bumpScale: 0.05,metalness: metalness}),
+      new three.MeshStandardMaterial({map:loader.load("./normal/plane.png"),bumpScale: 0.05,metalness: metalness}),
+      new three.MeshStandardMaterial({map:loader.load("./normal/normal_left.png"),bumpScale: 0.05,metalness: metalness}),
+      new three.MeshStandardMaterial({map:loader.load("./normal/plane.png"),bumpScale: 0.05,metalness: metalness}),
+    ]
+  
+    
+    const light_materials = [
+      new three.MeshStandardMaterial({map:loader.load("./light/light_top.png"),bumpMap: loader.load("./normal/normal_top.png"),bumpScale: 0.05,metalness: metalness,}),
+      new three.MeshStandardMaterial({map:loader.load("./light/light_bottom.png"),bumpMap: loader.load("./normal/normal_bottom.png"),bumpScale: 0.05,metalness: metalness,}),
+      new three.MeshStandardMaterial({map:loader.load("./light/light_front.png"),bumpMap: loader.load("./normal/normal_front.png"),bumpScale: 0.05,metalness: metalness,}),
+      new three.MeshStandardMaterial({map:loader.load("./light/plane.png"),bumpScale: 0.05,metalness: metalness,}),
+      new three.MeshStandardMaterial({map:loader.load("./light/light_left.png"),bumpScale: 0.05,metalness: metalness,}),
+      new three.MeshStandardMaterial({map:loader.load("./light/plane.png"),bumpScale: 0.05,metalness: metalness,}),
+    ]
+  
+    useFrame(() => {
+      boxRef.current!.rotation.y += speed
+      boxRef.current.rotation.x += speed
+      boxRef.current.material = materials
+      if(bool){
+        boxRef.current.material = light_materials
+      }
+    })
+  
+    const {scene}  = useThree()
+    scene.background=new three.Color("#222222")
+  
+    return (
+      <>
+        <mesh ref={boxRef} position={[0,1,0]} castShadow receiveShadow
+          onClick={(event) => {setbool(!bool)}}>
+          <boxGeometry args={[1,1,1]}/>
+          <meshStandardMaterial/>
+        </mesh>
+      </>
+    )
+  }
 
   const [active, setActive] = useState(false)
-
-  const materials = [
-    new three.MeshStandardMaterial({map:loader.load("./normal_top.png")}),
-    new three.MeshStandardMaterial({map:loader.load("./normal_bottom.png")}),
-    new three.MeshStandardMaterial({map:loader.load("./normal_front.png")}),
-    new three.MeshStandardMaterial({map:loader.load("./plane.png")}),
-    new three.MeshStandardMaterial({map:loader.load("./normal_left.png")}),
-    new three.MeshStandardMaterial({map:loader.load("./plane.png")}),
-  ]
-
-  const metalness = 2.0
-  const light_materials = [
-    new three.MeshStandardMaterial({map:loader.load("./light_top.png"),transparent: true,metalness: metalness,}),
-    new three.MeshStandardMaterial({map:loader.load("./light_bottom.png"),transparent: true,metalness: metalness,}),
-    new three.MeshStandardMaterial({map:loader.load("./light_front.png"),transparent: true,metalness: metalness,}),
-    new three.MeshStandardMaterial({map:loader.load("./plane.png"),transparent: true,metalness: metalness,}),
-    new three.MeshStandardMaterial({map:loader.load("./light_left.png"),transparent: true,metalness: metalness,}),
-    new three.MeshStandardMaterial({map:loader.load("./plane.png"),transparent: true,metalness: metalness,}),
-  ]
-
-  useFrame(() => {
-    boxRef.current!.rotation.y += 0.01
-
-  })
-
-  const {scene}  = useThree()
-  scene.background=new three.Color("#222222")
-
-  return (
-    <>
-      <mesh ref={boxRef} position={[0,1,0]} castShadow receiveShadow
-        onClick={(event) => setActive(!active)}>
-        <boxGeometry args={[1,1,1]}/>
-        <meshStandardMaterial
-          material={active ? light_materials : materials}/>
-      </mesh>
-    </>
-  )
-}
-
-function App() {
-  
+  const [speed,setSpeed] = useState(0.01)
   return (
     <>
       <Canvas
         camera={
           {
-            position: [0,5,8],
+            position: [0,3,2],
             fov: 50,
             aspect: window.innerWidth / window.innerHeight,
             near: 0.1,
@@ -66,7 +70,7 @@ function App() {
           shadows>
           <color attach="background" args={["#1e1e1e"]} />
           <OrbitControls />
-          <ambientLight intensity={0.1} />
+          <ambientLight intensity={0.2} />
           <directionalLight
             position={[5,5,5]}
             intensity={1}
@@ -78,9 +82,19 @@ function App() {
           <Plane rotation={[-Math.PI / 2, 0, 0]} args={[5, 5]} receiveShadow>
               <meshStandardMaterial color="#fff" side={three.DoubleSide} />
           </Plane>
-          <CubeObj1 />
+          <CubeObj1 bool={active} setbool={setActive} speed={speed}/>
         </Suspense>
       </Canvas>
+      <button onClick={() => setActive(!active)}>状態変化</button>
+      <div>
+        <p>回転速度変化</p>
+        <button onClick={() => setSpeed(speed - 0.01)}>
+          減速
+        </button>
+        <button onClick={() => setSpeed(speed + 0.01)}>
+          加速
+        </button>
+      </div>
     </>
   )
 }
